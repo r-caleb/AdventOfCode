420931dc84b551294aca00001bab5d7686f05a07
r-caleb <calebakasa7@gmail.com>
2022-12-02 11:50:09 +0100

    fix regex error

--- Day1/calorieCounting.js
+++ Day1/calorieCounting.js
@@ -2,13 +2,11 @@ const { readFileSync } = require("fs");
 // Fonction permettant la lecture du fichier et le replacement des sauts de ligne en espace vide
 function ReadFile(filename) {
   const contents = readFileSync(filename, "utf-8");
-
-  const arr = contents.replace(/\r\n/g, " ").split(" ");
+  const arr = contents.replace(/\n/g, " ").split(" ");
   return arr;
 }
 
 let inputClean = ReadFile("./input.txt");
-
 // fontion permettant de faire le groupage
 const makeGroups = (array) => {
   const groups = [];
@@ -24,10 +22,8 @@ const makeGroups = (array) => {
   groups.push(group);
   return groups;
 };
-
 let getGroups = makeGroups(inputClean);
-
-// Fonction permettant de faire la sommation des groupe
+// Fonction permettant de faire la sommation des groupes
 let totalPerGroups = [];
 const handleGroup = (groups) => {
   const totalCaloriesPerGroup = groups.reduce((prev, curr) => {
@@ -35,15 +31,14 @@ const handleGroup = (groups) => {
   }, 0);
   totalPerGroups.push(totalCaloriesPerGroup);
 };
-
-// Sommation des calories par groupe
-
 for (let i = 0; i < getGroups.length; i++) handleGroup(getGroups[i], i);
+// Part 1
 const largestTotal = Math.max(...totalPerGroups); // le plus grand total
+console.log(largestTotal);
 
 // Part 2
+
 const sortLargestTotal = totalPerGroups.sort((a, b) => b - a);
 const sumThreeLargestTotal =
   sortLargestTotal[0] + sortLargestTotal[1] + sortLargestTotal[2];
-
-
+console.log(sumThreeLargestTotal);
